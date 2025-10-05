070ac4772e6e2217707f4fe7439c53c1b06d07ca
Krahul1234 <kumarrahul21269@gmail.com>
2025-10-05 13:11:10 +0530

    Image Integration done

--- src/main/java/com/example/Search_project/controller/ChatController.java
+++ src/main/java/com/example/Search_project/controller/ChatController.java
@@ -1,20 +1,38 @@
 package com.example.Search_project.controller;
 
+import com.example.Search_project.service.ProductService;
+import jakarta.annotation.Resource;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @RestController
+@RequestMapping("/api/uploads")
 public class ChatController {
 private final ChatClient chatClient;
+
+@Value("${file.upload-dir}")
+private String uploadDir;
+@Autowired
+private ProductService productService;
 
     public ChatController(ChatClient.Builder builder) {
         this.chatClient = builder
                 .defaultSystem("You are an AI assistant answering questions about different products")
                 .build();
     }
+
 
 @GetMapping("generate")
     public ResponseEntity<?> generate(@RequestParam(value = "message") String message){
@@ -32,4 +50,33 @@ private final ChatClient chatClient;
          return ResponseEntity.internalServerError().body("An error occurred: " + e.getMessage());
      }
 }
-}
+  @PostMapping("image")
+    public ResponseEntity<String> saveImage(@RequestParam("file") MultipartFile file){
+    try{
+        String filePath = productService.saveImage(file);
+        return ResponseEntity.ok("Image uploaded successfully: " + filePath);
+    } catch (IOException e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error uploading image");
+    }
+    }
+
+    @GetMapping("/images/{filename}")
+    public ResponseEntity<Resource> getImageURL(@PathVariable String filename) {
+        try{
+            Path filePath = Paths.get(uploadDir).resolve(filename);
+            UrlResource resource = new UrlResource(filePath.toUri());
+
+            if (resource.exists()) {
+                return ResponseEntity.ok()
+                        .contentType(MediaType.IMAGE_JPEG)
+                        .body((Resource) resource);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (MalformedURLException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+  }
+
+
--- src/main/java/com/example/Search_project/service/ProductService.java
+++ src/main/java/com/example/Search_project/service/ProductService.java
@@ -2,8 +2,24 @@ package com.example.Search_project.service;
 
 import com.example.Search_project.entity.Product;
 import com.example.Search_project.repository.ProductRepository;
+import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 @Service
 public class ProductService {
@@ -11,11 +27,27 @@ public class ProductService {
     @Autowired
     private final ProductRepository productRepositery;
 
+    @Value("${file.upload-dir}")
+    private String uploadDir;
+
     public ProductService(ProductRepository productRepository) {
+
         this.productRepositery = productRepository;
     }
 
     public Product findProductByName(String name) {
         return productRepositery.findByNameIgnoreCase(name);
+    }
+
+
+    public String saveImage(MultipartFile file) throws IOException {
+        Path path = Paths.get(uploadDir);
+        if(!Files.exists(path)){
+            Files.createDirectories(path);
+        }
+        String fileName = file.getOriginalFilename();
+        Path filePath = path.resolve(fileName);
+        Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+        return filePath.toString();
     }
 }
